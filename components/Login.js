bcb2a9030949bea12d5db4c6a943f9f450b853d9
jaidan22 <jaidansleeba22@gmail.com>
2022-07-03 18:26:56 +0530

    fixed errors

--- components/Login.js
+++ components/Login.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { useMoralis } from "react-moralis";
 import logo from "../public/download.jpeg";
+import bg from "../public/bg.jpeg";
 
 const Login = () => {
   const { authenticate, isInitializing } = useMoralis();
@@ -25,11 +26,7 @@ const Login = () => {
       </div>
 
       <div className="w-full h-screen">
-        <Image
-          src="https://links.papareact.com/55n"
-          layout="fill"
-          objectFit="cover"
-        />
+        <Image src={bg} layout="fill" objectFit="cover" />
       </div>
     </div>
   );
